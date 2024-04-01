bf446225df886ce6c5e78c40952fb45828544256
Ashu Negi <anegi6723@gmail.com>
2024-04-01 10:49:41 +0530

    added methods to generate accessToken and Refresh token in userSchema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,67 +1,94 @@
-import mongoose, {Schema} from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 const userSchema = new Schema({
-    username : {
-        type : String,
-        required : true,
-        unique : true,
-        lowercase : true,
-        trim : true, // removes leading or trailing spaces
-        index : true // Indexes are essentially data structures that help MongoDB quickly locate documents based on specific field values.
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true, // removes leading or trailing spaces
+        index: true // Indexes are essentially data structures that help MongoDB quickly locate documents based on specific field values.
         // Creating an index on username can significantly improve performance when searching for users by username
     },
-    email : {
-        type : String,
-        required : true,
-        unique : true,
-        lowercase : true,
-        trim : true
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
     },
-    fullname : {
-        type : String,
-        required : true,
-        trim : true,
-        index : true
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
     },
-    avatar : {
-        type : String, // cloudinary url
-        required : true,
+    avatar: {
+        type: String, // cloudinary url
+        required: true,
     },
-    coverImage : {
-        type : String, // cloudinary url
+    coverImage: {
+        type: String, // cloudinary url
     },
-    watchHistory : [
+    watchHistory: [
         {
-            type : Schema.Types.ObjectId,
-            ref : "Video"
+            type: Schema.Types.ObjectId,
+            ref: "Video"
         }
     ],
-    password : {
-        type : String,
-        required : [true, "Password is required"]
+    password: {
+        type: String,
+        required: [true, "Password is required"]
     },
-    refreshToken : {
-        type : string
+    refreshToken: {
+        type: string
     }
 
-},{timestamps : true})
+}, { timestamps: true })
 
-userSchema.pre("save", async function(next){
+userSchema.pre("save", async function (next) {
     // password should only change if it is modified
-    if(!this.isModified("password")){
+    if (!this.isModified("password")) {
         return next();
     }
-    else{
+    else {
         this.password = await bcrypt.hash(this.password, 10);
         next();
     }
 })
 
 // adding methods to userSchema
-userSchema.methods.isPasswordCorrect = async function(password){
-    return await bcrypt.compare(password,this.password)
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
 }
 
 const User = mongoose.model("User", userSchema);
