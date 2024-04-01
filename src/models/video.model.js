36a9bf5886d0a0fa9f4b1c51775239affe31e8ff
Ashu Negi <anegi6723@gmail.com>
2024-04-01 09:55:33 +0530

    created video model

---
+++ src/models/video.model.js
@@ -0,0 +1,40 @@
+import mongoose, {Schema} from "mongoose";
+
+const videoSchema = new Schema({
+    videoFile : {
+        type : String, // cloudinary url
+        required : [true , "Video url is requied"]
+    },
+    thumbnail : {
+        type : String, // cloudinary url
+        required :  [true , "Thumbnail is requied"]
+    },
+    title : {
+        type : String, 
+        required :  [true , "Title is requied"]
+    },
+    description : {
+        type : String,
+        required :  [true , "Description is requied"]
+    },
+    duration : {
+        type : Number, // from cloudinary
+        required :  [true , "Duration is requied"]
+    },
+    views : {
+        type : Number,
+        default : 0,
+    },
+    isPublished : {
+        type : Boolean,
+        default : true
+    },
+    owner : {
+        type : Schema.Types.ObjectId,
+        ref : "User"
+    }
+},{timestamps : true})
+
+const Video = mongoose.model("Video", videoSchema);
+export default Video;
+
