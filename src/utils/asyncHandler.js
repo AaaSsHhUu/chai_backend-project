a504d7754a89415a2c6d3b50d7db3230cc30ec3d
Ashu Negi <anegi6723@gmail.com>
2024-04-04 06:30:19 +0530

    using approach 2

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,21 +1,21 @@
 //  ------------ Approach 1 ----------------
-const asyncHandler = (fn) => async (req,res,next) => {
-    try{
-        await fn(req,res,next);
-    }
-    catch(err){
-        res.status(err.code || 500).json({
-            sucess : false,
-            message : err.message
-        })
-    }
-}
-
-//  ------------ Approach 2 ----------------
-// const  asyncHandler = (requestHandler) => {
-//     (req,res,next) => {
-//         Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
+// const asyncHandler = (fn) => async (req,res,next) => {
+//     try{
+//         await fn(req,res,next);
+//     }
+//     catch(err){
+//         res.status(err.code || 500).json({
+//             sucess : false,
+//             message : err.message
+//         })
 //     }
 // }
+
+//  ------------ Approach 2 ----------------
+const  asyncHandler = (requestHandler) => {
+    return (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
+    }
+}
 
 export default asyncHandler;
