a2d0f06dba35a96ae11b63a85cbedf1a46f7904e
Ashu Negi <anegi6723@gmail.com>
2024-04-12 12:13:51 +0530

    added controller to get current user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -220,4 +220,15 @@ const changePassword = asyncHandler( async (req,res) => {
     )
 })
 
+const getCurrentUser = asyncHandler(async (req,res) => {
+    return res.status(200)
+            .json(
+                new ApiResponse(
+                    200,
+                    req.user,
+                    "Current user fetched successfully"
+                )
+            )
+})
+
 export { registerUser, loginUser, logoutUser, refreshAccessToken };
